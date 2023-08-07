831c84df252ea80a0f9b433cf5b50989c68d444d
4FunkE <4FunkEcodes@gmail.com>
2023-08-07 12:53:37 -0400

    created tables based on criteria now to connect primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,35 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- Creates the table "department" within employee_db
+CREATE TABLE department (
+  -- Creates a numeric column called "id"
+  id INT PRIMARY KEY,
+  -- Makes a string column up to 30 characters called "name" which cannot contain null
+  name VARCHAR(30) NOT NULL
+);
+
+-- Creates the table "role" within employee_db
+CREATE TABLE role (
+  -- Creates a numeric column called "id"
+  id INT PRIMARY KEY,
+  -- Makes a string column up to 30 characters called "title" which cannot contain null
+  title VARCHAR(30) NOT NULL
+--   Makes a column called "salary" entered in decimal form
+  salary DECIMAL
+--   Makes a column called "department_id" entered as an interger
+  department_id INT
+);
+
+-- Creates the table "employee" within employee_db
+CREATE TABLE employee (
+  -- Creates a numeric column called "id"
+  id INT PRIMARY KEY,
+  -- Makes a string column up to 30 characters called "first_name" which cannot contain null
+  first_name VARCHAR(30) NOT NULL
+  -- Makes a string column up to 30 characters called "last_name" which cannot contain null
+  last_name VARCHAR(30) NOT NULL
+  -- Makes a column called "role_id" entered as an interger
+  role_id INT
+  -- Makes a column called "manager_id" entered as an interger, null if employye has no manager
+  manager_id INT 
+);
