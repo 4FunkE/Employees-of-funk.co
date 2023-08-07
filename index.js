c31a6e224d521dab6b9787f8de6d580791ee5ec2
4FunkE <4FunkEcodes@gmail.com>
2023-08-07 14:09:26 -0400

    Main Menu off criteria video

--- index.js
+++ index.js
@@ -15,5 +15,33 @@ const db = mysql.createConnection(
       password: process.env.DB_password,
       database: 'employee_db'
     },
-    console.log(`Connected to the courses_db database.`)
+    console.log(`Connected to the employee database.`)
   );
+
+// Connect to the database
+db.connect((err) => {
+    if (err) throw err;
+    // Call the function to display the main menu
+    mainMenu();
+  });
+
+// Display the main menu
+function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Exit',
+          ],
+        },
+      ])
