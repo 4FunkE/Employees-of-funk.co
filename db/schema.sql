49726d44c8568766b1c6acefdc6543ad6f619a15
4FunkE <4FunkEcodes@gmail.com>
2023-08-07 12:38:00 -0400

    creating schema, using activities to elp scaffold

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- drop db to create db
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+-- Uses the employee_db database
+USE employee_db;
+
+-- Creates the table "department" within employee_db
